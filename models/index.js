e6383fd0ac09d6d5fe9ffffd2729725f168e39a4
Joey V <joey03v@gmail.com>
2023-12-15 14:21:40 -0600

    finished index.js model relationships

--- models/index.js
+++ models/index.js
@@ -6,21 +6,23 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  
+  foreignKey: 'category_id'
 })
 
 // Categories have many Products
 Category.hasMany(Product, {
-
+  foreignKey: 'category_id'
 })
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(ProductTag, {
-
+  foreignKey: 'product_id',
+  through: ProductTag
 })
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-
+  foreignKey: 'tag_id',
+  through: ProductTag
 })
 
 
